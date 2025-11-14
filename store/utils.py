9d7926a2ad1d8919bc7a2eca860a9fa5c294b07f
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-14 22:19:42 +0100

    Add helper utilities for generating and decoding email verification tokens

--- store/utils.py
+++ store/utils.py
@@ -2,7 +2,9 @@
 from django.core.cache import cache
 from .models import Product
 from .serializers import ProductSerializer
-
+import jwt
+from datetime import datetime, timedelta
+from django.conf import settings
 def get_all_products():
     cached_products=cache.get("product_list")
     if cached_products is not None:
@@ -20,3 +22,13 @@ def get_all_products():
         "message": "Properties fetched successfully",
         "data": serialized_product
     }
+
+def generate_email_token(user):
+    payload = {
+        'user_id': user.id,
+        'email':user.email,
+        'exp': datetime.utcnow() + timedelta(days=1),
+        'type':'email_confirmation'
+    
+    }
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
